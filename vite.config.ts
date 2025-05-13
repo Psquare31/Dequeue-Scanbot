072b2cc814d98b634105e6072a868d876b1c2bc4
Psquare <pranavprajyot31@gmail.com>
2025-05-13 10:45:51 +0530

    Debug test

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,10 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  server: {
-    proxy: {
-      '/api': 'https://deque-scanbot-backend-epn9.vercel.app',
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     '/api': 'https://deque-scanbot-backend-epn9.vercel.app',
+  //   },
+  // },
 
 });
